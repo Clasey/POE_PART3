af52078f655fe64ae50816a61c5f388a72127b3f
Clasey <claseyk@gmail.com>
2024-11-17 18:29:26 +0200

    Updating and adding ClaimViewModel and setting up getters and setters for collecting information.

---
+++ Models/ClaimViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace POE_PART3.Models
+{
+    public class ClaimViewModel
+    {
+        public string LecturerName { get; set; }
+        public decimal HoursWorked { get; set; }
+        public decimal HourlyRate { get; set; }
+        public string Notes { get; set; }
+        public IFormFile SupportingDocument { get; set; }
+    }
+}
