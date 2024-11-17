7a4a6472847f7df6f773bb45b8a63c5cd2b0caf2
Clasey <claseyk@gmail.com>
2024-11-17 16:34:16 +0200

    Updating the MainWindow.xaml.cs file

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,16 @@
-﻿namespace POE_PART3
+﻿using System.Windows;
+using POE_PART3.Views;
+using POE_PART3.Models;
+using POE_PART3.Controllers;
+
+namespace POE_PART3
 {
     public class MainWindow
     {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainFrame.Navigate(new LoginView());
+        }
     }
 }
