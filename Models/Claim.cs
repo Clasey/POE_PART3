47de65277ad6dd7a66e9a028b5d44746288c518e
Clasey <claseyk@gmail.com>
2024-11-17 15:53:11 +0200

    Add project files.

---
+++ Models/Claim.cs
@@ -0,0 +1,14 @@
+﻿namespace POE_PART3.Models
+{
+    public class Claim
+    {
+        public int ClaimId { get; set; }
+        public string LecturerName { get; set; }
+        public DateTime DateSubmitted { get; set; }
+        public double HoursWorked { get; set; }
+        public double HourlyRate { get; set; }
+        public double TotalAmount => HoursWorked * HourlyRate;
+        public string Status { get; set; } = "Pending";
+
+    }
+}
